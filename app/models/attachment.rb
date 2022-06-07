bfd63339291f7a473d99ce5b35b6138cde7d360a
shalapa1ka <flecks07@gmail.com>
2022-06-07 15:22:17 +0300

    Do dynamic form for add files

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,6 +1,7 @@
 # frozen_string_literal: true
 
 class Attachment < ApplicationRecord
+  validates :file, presence: true
   belongs_to :attachable, polymorphic: true
 
   mount_uploader :file, FileUploader
