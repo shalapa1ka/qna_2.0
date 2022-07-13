99992b6f7dbf3d9ae50e7b030e8bfe353408ad5c
shalapa1ka <flecks07@gmail.com>
2022-07-13 14:47:39 +0300

    Fix credentials for AWS

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -6,11 +6,11 @@ CarrierWave.configure do |config|
 
   config.fog_credentials = {
     provider: 'AWS',
-    aws_access_key_id: Rails.application.credentials.aws.access_key,
-    aws_secret_access_key: Rails.application.credentials.aws.secret_key,
+    aws_access_key_id: Rails.application.credentials.aws[:access_key],
+    aws_secret_access_key: Rails.application.credentials.aws[:secret_key],
     region: 'us-east-1'
   }
-  config.fog_directory = Rails.application.credentials.aws.bucket
+  config.fog_directory = Rails.application.credentials.aws[:bucket]
   config.fog_public = false
   config.cache_dir = "#{Rails.root}/tmp/uploads"
   config.storage = :fog
